cd56c8651b743c126a451c2049ff2a899dde8a1f
Nagy-Zsugya Martin Albert <zsugya.martin@gmail.com>
2022-08-08 17:57:25 +0200

    Implement very basic validation

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
 let myLibrary = [];
-const bookshelf = document.querySelector('.bookshelf');
-const newBook = document.querySelector('.new-book');
-const addBookForm = document.querySelector('.add-book-form');
-const addBook = document.querySelector('.add-book');
-const formTitle = document.querySelector('#title');
-const formAuthor = document.querySelector('#author');
-const formPages = document.querySelector('#pages');
-const formRead = document.querySelector('#read');
+const bookshelf = document.querySelector(".bookshelf");
+const newBook = document.querySelector(".new-book");
+const addBookForm = document.querySelector(".add-book-form");
+const addBook = document.querySelector(".add-book");
+const formTitle = document.querySelector("#title");
+const formAuthor = document.querySelector("#author");
+const formPages = document.querySelector("#pages");
+const formRead = document.querySelector("#read");
 
-newBook.addEventListener('click', () => {
-    addBookForm.hidden = !addBookForm.hidden;
-})
-addBook.addEventListener('click', () => {
-    addBookToLibrary(formTitle.value, formAuthor.value, formPages.value, formRead.checked);
-    console.log(formRead.value);
-    displayBooks();
-})
+newBook.addEventListener("click", () => {
+  addBookForm.hidden = !addBookForm.hidden;
+});
+addBook.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    formTitle.validity.valid &&
+    formAuthor.validity.valid &&
+    formPages.validity.valid &&
+    formRead.validity.valid
+  )
+    addBookToLibrary(
+      formTitle.value,
+      formAuthor.value,
+      formPages.value,
+      formRead.checked
+    );
+  displayBooks();
+});
 
-class Book{
-    constructor(title, author, pages, read){
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
-    info(){
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}`;
-    }
-    toggleRead(){
-        this.read = !this.read;
-    }
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  info() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.read ? "already read" : "not read yet"
+    }`;
+  }
+  toggleRead() {
+    this.read = !this.read;
+  }
 }
 
 // function Book(title, author, pages, read){
@@ -48,45 +61,45 @@ class Book{
 //     this.read = !this.read;
 // }
 
-function addBookToLibrary(title, author, pages, read){
-    myLibrary.push(new Book(title, author, pages, read));
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
 }
 
 addBookToLibrary("Hobbit", "JRR Tolkien", 250, true);
 addBookToLibrary("Lord of the Rings", "JRR Tolkien", 800, false);
 addBookToLibrary("Game of Thrones", "George RR Martin", 5000, true);
 
-function displayBooks(){
-    bookshelf.innerHTML = '';
-    for(let i = 0; i < myLibrary.length; i++){
-        let bookCard = document.createElement('div');
-        bookCard.classList.add('book');
-        bookCard.dataset.number = i;
-        bookCard.textContent = myLibrary[i].info();
+function displayBooks() {
+  bookshelf.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let bookCard = document.createElement("div");
+    bookCard.classList.add("book");
+    bookCard.dataset.number = i;
+    bookCard.textContent = myLibrary[i].info();
 
-        let xBtn = document.createElement('button');
-        xBtn.classList.add('xBtn');
-        xBtn.textContent = 'X';
-        xBtn.dataset.number = i;
-        xBtn.addEventListener('click', (e) => {
-            myLibrary.splice(e.target.dataset.number, 1);
-            displayBooks();
-        })
-        
-        let toggleBtn = document.createElement('button');
-        toggleBtn.textContent = 'Read book';
-        toggleBtn.classList.add('toggleBtn');
-        toggleBtn.dataset.number = i;
-        toggleBtn.addEventListener('click', (e) => {
-            console.log(e.target.dataset.number);
-            myLibrary[e.target.dataset.number].toggleRead();
-            displayBooks();
-        });
+    let xBtn = document.createElement("button");
+    xBtn.classList.add("xBtn");
+    xBtn.textContent = "X";
+    xBtn.dataset.number = i;
+    xBtn.addEventListener("click", (e) => {
+      myLibrary.splice(e.target.dataset.number, 1);
+      displayBooks();
+    });
 
-        bookCard.appendChild(toggleBtn);
-        bookCard.appendChild(xBtn);
-        bookshelf.appendChild(bookCard);
-    }
+    let toggleBtn = document.createElement("button");
+    toggleBtn.textContent = "Read book";
+    toggleBtn.classList.add("toggleBtn");
+    toggleBtn.dataset.number = i;
+    toggleBtn.addEventListener("click", (e) => {
+      console.log(e.target.dataset.number);
+      myLibrary[e.target.dataset.number].toggleRead();
+      displayBooks();
+    });
+
+    bookCard.appendChild(toggleBtn);
+    bookCard.appendChild(xBtn);
+    bookshelf.appendChild(bookCard);
+  }
 }
 
 displayBooks();
